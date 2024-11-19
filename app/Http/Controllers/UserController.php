13a3cf83a6d6d90c4c0108d02286642bd254b7fb
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-11-19 10:49:57 +0100

    validation for UserController store() & update()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -12,35 +13,71 @@ class UserController extends Controller
     {
         $response = Http::get('https://jsonplaceholder.typicode.com/users');
 
-        return response()->json($response->json());
+        if($response->successful()){
+            return response()->json($response->json());
+        }
+
+        return response()->json([]);
     }
 
     // Create a new user (id: 11 always)
-    public function store()
+    public function store(Request $request)
     {
         $data = [
-          'id' => 1,
-          'name' => 'Name',
-          'username' => 'Username',
-          'email' => 'name_username@gmail.com',
-          'address' => [
-              'street' => 'Street test',
-              'suite' => 'Suite test',
-              'city' => 'Test. 000',
-              'zipcode' => 00000-0000,
-              'geo' => [
-                  'lat' => '-00.0000',
-                  'lng' => '00.0000',
-              ],
-          ],
+            'id' => 1,
+            'name' => 'Name',
+            'username' => 'Username',
+            'email' => 'name_username@gmail.com',
+
+            'address' => [
+                'street' => 'Street test',
+                'suite' => 'Suite test',
+                'city' => 'Test. 000',
+                'zipcode' => '00000-0000',
+                'geo' => [
+                    'lat' => -00.0000,
+                    'lng' => 00.0000,
+                ],
+            ],
+
             'phone' => '0-000-000-0000 x00000',
-            'website' => 'test.org',
+            'website' => 'https://test.com/',
+
             'company' => [
                 'name' => 'Test-Company',
                 'catchPhrase' => 'Multi-testing client-server',
                 'bs' => 'harness real-time e-tests',
             ],
         ];
+
+        $validator = Validator::make($data, [
+            'name' => 'required|min:2|max:50',
+            'username' => 'required|min:2|max:50',
+            'email' => 'required|email|max:50',
+
+            'address' => 'array',
+                'address.street' => 'min:5',
+                'address.suite' => 'min:5',
+                'address.city' => 'min:5',
+                'address.zipcode' => 'max:50',
+                'address.geo.lat' => 'numeric',
+                'address.geo.lng' => 'numeric',
+
+            'phone' => 'required|max:50',
+            'website' => 'required|url',
+
+            'company' => 'array',
+                'company.name' => 'required|max:50',
+                'company.catchPhrase' => 'required|max:50',
+                'company.bs' => 'required|max:50'
+        ]);
+
+        // Check validation error
+        if ($validator->fails()){
+            return response()->json([
+               'error' => $validator->errors()
+            ], 422);
+        }
 
         $response = Http::post('https://jsonplaceholder.typicode.com/users', $data);
 
@@ -74,13 +111,42 @@ class UserController extends Controller
                 ],
             ],
             'phone' => '0-000-000-0000 x00000',
-            'website' => 'test_updated.org',
+            'website' => 'https://test.com/',
             'company' => [
                 'name' => 'Test-Company updated',
                 'catchPhrase' => 'Multi-testing client-server updated',
                 'bs' => 'harness real-time e-tests updated',
             ],
         ];
+
+        $validator = Validator::make($data, [
+            'name' => 'required|min:2|max:50',
+            'username' => 'required|min:2|max:50',
+            'email' => 'required|email|max:50',
+
+            'address' => 'array',
+            'address.street' => 'min:5',
+            'address.suite' => 'min:5',
+            'address.city' => 'min:5',
+            'address.zipcode' => 'max:50',
+            'address.geo.lat' => 'numeric',
+            'address.geo.lng' => 'numeric',
+
+            'phone' => 'required|max:50',
+            'website' => 'required|url',
+
+            'company' => 'array',
+            'company.name' => 'required|max:50',
+            'company.catchPhrase' => 'required|max:50',
+            'company.bs' => 'required|max:50'
+        ]);
+
+        // Check validation error
+        if ($validator->fails()){
+            return response()->json([
+                'error' => $validator->errors()
+            ], 422);
+        }
 
         $response = Http::put('https://jsonplaceholder.typicode.com/users/' . $id, $data);
 
